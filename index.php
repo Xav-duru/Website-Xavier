fcb636636287931ebcc6ba3a2b6cb9b2264ab557
Xav-duru <xavier.durumain@orange.fr>
2023-07-26 12:50:58 +0200

    Create form

--- index.php
+++ index.php
@@ -3,19 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SiteWeb Xavier</title>
 </head>
 <body>
-    <div></div>
-
-
     <form method="post">
-        <input type="text" name="pseudo" id="pseudo" placeholder="Ton pseudo">
-        <input type="password" name="password" id="password" placeholder="Mot de passe">
-        <input type="submit" name="formsend" id="formsend">
+        <input type="text" name="pseudo" id="pseudo" placeholder="Ton pseudo" require>
+        <input type="password" name="password" id="password" placeholder="Mot de passe" require>
+        <input type="submit" name="formsend" id="formsend" disabled>
     </form>
     
+    <?php
     
+    if(isset($_POST['formsend']) && (!empty($_POST['pseudo']))){
+        $pseudo = $_POST['pseudo'];
+        $password = $_POST['password'];
+        if(!empty($pseudo) && !empty($password)){
+            echo "Bienvenue " . $pseudo . " !";
+        }
+    }
+    
+    ?>
+
 
 </body>
 </html>
