5e05b092ede721fd10de85eb3cb9986f48877de0
Xav-duru <xavier.durumain@orange.fr>
2023-07-26 11:28:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div></div>
+
+
+    <form method="post">
+        <input type="text" name="pseudo" id="pseudo" placeholder="Ton pseudo">
+        <input type="password" name="password" id="password" placeholder="Mot de passe">
+        <input type="submit" name="formsend" id="formsend">
+    </form>
+    
+    
+
+</body>
+</html>
