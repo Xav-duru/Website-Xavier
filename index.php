8a95292b13906b803a16b18e6e08b7c412e3a8f4
Xav-duru <xavier.durumain@orange.fr>
2023-07-28 10:22:53 +0200

    Ajouts fichiers

--- index.php
+++ index.php
@@ -9,17 +9,31 @@
     <form method="post">
         <input type="text" name="pseudo" id="pseudo" placeholder="Ton pseudo" require>
         <input type="password" name="password" id="password" placeholder="Mot de passe" require>
-        <input type="submit" name="formsend" id="formsend" disabled>
+        <input type="submit" name="formsend" id="formsend">
+        <?php
+        /*
+        if(!empty($pseudo) && !empty($password)){
+            submit;
+        }
+        */
+        ?>
     </form>
     
     <?php
-    
+    $psuedoValidate = "xav";
+    $passwordValidate = "azerty";
     if(isset($_POST['formsend']) && (!empty($_POST['pseudo']))){
         $pseudo = $_POST['pseudo'];
         $password = $_POST['password'];
+        header('location:page.php');
+        /*
         if(!empty($pseudo) && !empty($password)){
+            if($pseudo==$psuedoValidate && $password==$passwordValidate){
+                echo "Bienvenue " . $pseudo . " !";
+            }
             echo "Bienvenue " . $pseudo . " !";
         }
+        */
     }
     
     ?>
---
+++ page.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Xavier website</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/style_page.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    
+    <!-- Header -->
+    <header>
+        <h1>
+            Welcome to my website !
+        </h1>
+    </header>
+
+
+
+    <!-- Navigation menu -->
+    <nav class="menu-nav">
+        <ul>
+            <li class="btn">
+                <a href="https://www.linkedin.com/in/xavier-durumain/" target="_blank">
+                    Linkedin
+                </a>
+            </li>
+            <li class="btn">
+                <a href="CV.html" target="_blank">
+                    Resume
+                </a>
+            </li>
+            <li class="btn">
+                <a href="trek.html" target="_">
+                    Trek adventures
+                </a>
+            </li>
+            <li class="btn">
+                <a href="suede.html" target="_">
+                    Suède
+                </a>
+            </li>
+        </ul>
+    </nav>
+    
+    
+    <!-- The content -->
+    <div id="content">
+        <div class = "leftbox">
+            <button href="football.html" target="_blank">
+                Football
+            </button>
+        </div>
+
+        <div class="rightbox">
+            <button href="cyclisme.html" target="_blank">
+                Cyclisme
+            </button>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <footer>
+        Hope you enjoyed
+    </footer>
+</body>
+</html>
+
+
