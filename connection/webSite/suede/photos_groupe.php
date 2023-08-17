bf94105de924b0a4514048896f82db9a9303f069
Xav-duru <xavier.durumain@orange.fr>
2023-08-17 17:56:45 +0200

    add download link

--- connection/webSite/suede/photos_groupe.php
+++ connection/webSite/suede/photos_groupe.php
@@ -6,148 +6,131 @@
 <body>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/01.jpg">
         <img src="Photos/01.jpeg" alt="Cinque Terre">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/01.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
     
 
     <div class="gallery">
-    <a target="_blank" href="Photos/02.jpg">
         <img src="Photos/02.jpeg" alt="Forest">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/02.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/03.jpg">
         <img src="Photos/03.jpeg" alt="Northern Lights">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/03.jpeg" id="download" download>Download</a><br><br>
+
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/04.jpg">
         <img src="Photos/04.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/04.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/05.jpeg">
         <img src="Photos/05.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/06.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/06.jpeg">
         <img src="Photos/06.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/06.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/07.jpeg">
         <img src="Photos/07.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/07.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/08.jpeg">
         <img src="Photos/08.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/08.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/09.jpeg">
         <img src="Photos/09.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/09.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/10.jpeg">
         <img src="Photos/10.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/10.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/11.jpeg">
         <img src="Photos/11.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/11.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/12.jpeg">
         <img src="Photos/12.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/12.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/13.jpeg">
         <img src="Photos/13.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/13.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/14.jpeg">
         <img src="Photos/14.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/14.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/15.jpeg">
         <img src="Photos/15.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/15.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
 
     <div class="gallery">
-    <a target="_blank" href="Photos/16.jpeg">
         <img src="Photos/16.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/16.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/17.jpeg">
         <img src="Photos/17.jpeg" alt="Mountains">
-    </a>
     <div class="desc">Add a description of the image here</div>
+    <a href="Photos/17.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
     <div class="gallery">
-    <a target="_blank" href="Photos/18.jpeg">
         <img src="Photos/18.jpeg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
+        <div class="desc">Add a description of the image here</div>
+        <a href="Photos/18.jpeg" id="download" download>Download</a><br><br>
     </div>
     <br><br>
 
