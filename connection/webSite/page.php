420bb0f452c6f038368014273fd7a5f7748ed774
Xav-duru <xavier.durumain@orange.fr>
2023-08-18 12:40:49 +0200

    New Page interface done

--- connection/webSite/page.php
+++ connection/webSite/page.php
@@ -11,66 +11,55 @@
 <body>
     
     <!-- Header -->
-    <header>
-        <h1>
-            Welcome to my website !
-        </h1>
-    </header>
+    
 
     <!-- Navigation menu -->
-    <nav class="menu-nav">
-        <ul>
-            <li class="btn">
-                <a href="https://www.linkedin.com/in/xavier-durumain/" target="_blank">
-                    Linkedin
-                </a>
-            </li>
-            <li class="btn">
-                <a href="CV.html" target="_blank">
-                    Resume
-                </a>
-            </li>
-            <li class="btn">
-                <a href="trek.html" target="_">
-                    Trek adventures
-                </a>
-            </li>
-            <li class="btn">
-                <a href="suede/suede.php" target="_">
-                    Suède
-                </a>
-            </li>
-            <li class="btn">
-                <a href="football.php" target="_">
-                    football
-                </a>
-            </li>
-            <li class="btn">
-                <a href="trek.html" target="_">
-                    Cyclisme
-                </a>
-            </li>
-        </ul>
-    </nav>
-    
-    <?php
-    /*
-    <!-- The content -->
-    <div id="content">
-        <div class = "leftbox">
-            <button href="football.html" target="_blank">
-                Football
-            </button>
+    <div class="menu-nav">
+        <div class="btn">
+            <div class="profil">
+                <h2>DURUMAIN<h2>
+                <h3>Xavier<h3>
+                <img src="Photo_Xav.jpg">
+            </div>
         </div>
-
-        <div class="rightbox">
-            <button href="cyclisme.html" target="_blank">
+        <br>
+        <div class="btn">
+            <a href="https://www.linkedin.com/in/xavier-durumain/" style="padding: 19px 119px 19px 118px;" target="_blank">
+                Linkedin
+            </a>
+        </div>
+        <br><br>
+        <div class="btn">
+            <a href="CV.html" style="padding: 19px 122px 19px 123px;" target="_blank">
+                Resume
+            </a>
+        </div>
+        <br><br>
+        <div class="btn">
+            <a href="trek.html" style="padding: 19px 94px 19px 94px;" target="_">
+                Trek adventures
+            </a>
+        </div>
+        <br><br>
+        <div class="btn">
+            <a href="suede/suede.php" style="padding: 19px 123px 19px 124px;" target="_">
+                Sweden
+            </a>
+        </div>
+        <br><br>
+        <div class="btn">
+            <a href="football.php" style="padding: 19px 123px 19px 124px;" target="_">
+                football
+            </a>
+        </div>
+        <br><br>
+        <div class="btn">
+            <a href="trek.html" style="padding: 19px 119px 19px 118px;" target="_">
                 Cyclisme
-            </button>
+            </a>
         </div>
+    
     </div>
-    */
-    ?>
 
     <!-- footer -->
     <footer>
--- connection/webSite/suede/photos_groupe.php
+++ connection/webSite/suede/photos_groupe.php
@@ -7,130 +7,130 @@
 
     <div class="gallery">
         <img src="Photos/01.jpeg" alt="Cinque Terre">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/01.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/01.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
     
 
     <div class="gallery">
         <img src="Photos/02.jpeg" alt="Forest">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/02.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/02.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/03.jpeg" alt="Northern Lights">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/03.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/03.jpeg" id="download" download>Download</a><br>
 
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/04.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/04.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/04.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/05.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/06.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/06.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/06.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/06.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Botanical Garden</div>
+        <a href="Photos/06.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/07.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/07.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Our favorite lake</div>
+        <a href="Photos/07.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/08.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/08.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Our favorite lake</div>
+        <a href="Photos/08.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/09.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/09.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Our favorite lake</div>
+        <a href="Photos/09.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/10.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/10.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Stockholm</div>
+        <a href="Photos/10.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/11.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/11.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Stockholm</div>
+        <a href="Photos/11.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/12.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/12.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Stockholm</div>
+        <a href="Photos/12.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/13.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/13.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Shot bar</div>
+        <a href="Photos/13.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/14.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/14.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Fjällnora naturreservat</div>
+        <a href="Photos/14.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/15.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/15.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Fjällnora naturreservat</div>
+        <a href="Photos/15.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
 
     <div class="gallery">
         <img src="Photos/16.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/16.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Fjällnora naturreservat</div>
+        <a href="Photos/16.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/17.jpeg" alt="Mountains">
-    <div class="desc">Add a description of the image here</div>
-    <a href="Photos/17.jpeg" id="download" download>Download</a><br><br>
+    <div class="desc">Fjällnora naturreservat</div>
+    <a href="Photos/17.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
     <div class="gallery">
         <img src="Photos/18.jpeg" alt="Mountains">
-        <div class="desc">Add a description of the image here</div>
-        <a href="Photos/18.jpeg" id="download" download>Download</a><br><br>
+        <div class="desc">Fjällnora naturreservat</div>
+        <a href="Photos/18.jpeg" id="download" download>Download</a><br>
     </div>
     <br><br>
 
