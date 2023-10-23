7e8ffc75716e8a56ae6808d46da9baaced1793e8
Xav-duru <xavier.durumain@orange.fr>
2023-10-23 22:56:55 +0200

    drag done

--- connection/webSite/football/coach/createCompo.php
+++ connection/webSite/football/coach/createCompo.php
@@ -5,80 +5,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' type='text/css' media='screen' href='createCompo.css'>
 
-    <title>Document</title>
+    <title>Coaching</title>
 </head>
 <body>
     <div class="leftBox">
     <h1 style="text-align:center">Create your own compo</h1>
-        <canvas id="monCanevas" width="600" height="800" style="border-radius: 5%;"></canvas>
+        <canvas id="idMyCanvas" width="550" height="750" style="border-radius: 5%;"></canvas>
         <div id="id1">
-            <button type="button" name="player" id="idBtn" class="widget" draggable="true" ondragend=true style="position: absolute; top: 750px; left: 240px;"> </button>
+            <button type="button" id="idBtn1" class="widget" style="position: absolute; top: 720px; left: 240px;"> </button>
+            <p id="idName1" style="position: absolute; top: 750px; left: 240px;">1. Neuer</p>
         </div>
         <div id="id2">
-            <button type="button" name="player" id="idBtn" class="widget" draggable="true" ondragend=true style="position: absolute; top: 550px; left: 70px;"></button>
+            <button type="button" id="idBtn2" class="widget" style="position: absolute; top: 550px; left: 70px;"></button>
+            <p id="idName2" style="position: absolute; top: 580px; left: 70px;">2. Neuer</p>
         </div>
         <div id="id3">
-            <button type="button" name="player" id="idBtn" class="widget" draggable="true" ondragend=true style="position: absolute; top: 600px; left: 180px;"></button>
+            <button type="button" id="idBtn3" class="widget" style="position: absolute; top: 600px; left: 180px;"></button>
+            <p id="idName3" style="position: absolute; top: 630px; left: 180px;">3. Neuer</p>
         </div>
         <div id="id4">
-            <button type="button" name="player" id="idBtn" class="widget" draggable="true" ondragend=true style="position: absolute; top: 600px; left: 300px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 600px; left: 300px;"></button>
         </div>
         <div id="id5">
-            <button type="button" name="player" id="idBtn" class="widget" draggable="true" ondragend=true style="position: absolute; top: 550px; left: 410px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 550px; left: 410px;"></button>
         </div>
         <div id="id6">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 450px; left: 180px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 450px; left: 180px;"></button>
         </div>
         <div id="id7">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 450px; left: 300px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 450px; left: 300px;"></button>
         </div>
         <div id="id8">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 340px; left: 240px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 340px; left: 240px;"></button>
         </div>
         <div id="id9">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 250px; left: 70px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 250px; left: 70px;"></button>
         </div>
         <div id="id10">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 250px; left: 410px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 250px; left: 410px;"></button>
         </div>
         <div id="id11">
-            <button type="button" name="player" id="idBtn" class="widget" style="position: absolute; top: 200px; left: 240px;"></button>
+            <button type="button" id="idBtn" class="widget" style="position: absolute; top: 200px; left: 240px;"></button>
         </div>
 
     </div>
     <div class="rightBox">
         <h2>Titulaires</h2>
-        <div id="id1">
+        <div id="idName1">
             <p>1. </p>
         </div>
-        <div id="id2">
+        <div id="idName2">
             <p>2. </p>
         </div>
-        <div id="id3">
+        <div id="idName3">
             <p>3. </p>
         </div>
-        <div id="id4">
+        <div id="idName4">
             <p>4. </p>
         </div>
-        <div id="id5">
+        <div id="idName5">
             <p>5. </p>
         </div>
-        <div id="id6">
+        <div id="idName6">
             <p>6. </p>
         </div>
-        <div id="id7">
+        <div id="idName7">
             <p>7. </p>
         </div>
-        <div id="id8">
+        <div id="idName8">
             <p>8. </p>
         </div>
-        <div id="id9">
+        <div id="idName9">
             <p>9. </p>
         </div>
-        <div id="id10">
+        <div id="idName10">
             <p>10. </p>
         </div>
-        <div id="id11">
+        <div id="idName11">
             <p>11. </p>
         </div>
         <br><br>
@@ -102,7 +105,7 @@
     </div>
 
     <script>
-        const canevas = document.getElementById("monCanevas");
+    const canevas = document.getElementById("idMyCanvas");
     const context = canevas.getContext("2d");
 
     const image = new Image();
@@ -113,6 +116,7 @@
         // Vous pouvez ajuster les coordonnées (0, 0) et les dimensions (canevas.width, canevas.height) selon vos besoins.
     };
 
+    /*
     const widgets = document.querySelectorAll('.widget');
     const field = document.getElementById("idField");
     let isDragging = false;
@@ -134,13 +138,11 @@
             const x = e.clientX - offsetX;
             const y = e.clientY - offsetY;
             // Limiter les déplacements du widget dans l'image de fond
-            //const maxX = field.offsetWidth - currentWidget.offsetWidth;
-            //const maxY = field.offsetHeight+50 - currentWidget.offsetHeight;
             
-            if (x >= 0 && x <= 700) {
+            if (x >= 47 && x <= canevas.width-90) {
                 currentWidget.style.left = x + "px";
             }
-            if (y >= 75 && y <= 700) {
+            if (y >= 120 && y <= canevas.height-10) {
                 currentWidget.style.top = y + "px";
             }
         }
@@ -152,34 +154,55 @@
             isDragging = false;
         }
     });
-    /*
-    widget.addEventListener("mousedown", (e) => {
-        isDragging = true;
-        offsetX = e.clientX - widget.getBoundingClientRect().left;
-        offsetY = e.clientY - widget.getBoundingClientRect().top;
-    });
-
-    document.addEventListener("mousemove", (e) => {
-        if (isDragging) {
-            const x = e.clientX - offsetX;
-            const y = e.clientY - offsetY;
-            // Limiter les déplacements du widget dans l'image de fond
-            const maxX = field.offsetWidth - widget.offsetWidth;
-            const maxY = field.offsetHeight+50 - widget.offsetHeight;
-
-            if (x >= 0 && x <= maxX) {
-            widget.style.left = x + "px";
+    */
+        class WidgetPair {
+            constructor(widget, follower) {
+                this.widget = widget;
+                this.follower = follower;
+                widget.addEventListener("mousedown", this.startDragging.bind(this));
             }
-            if (y >= 75 && y <= maxY) {
-            widget.style.top = y + "px";
+
+            startDragging(e) {
+                this.currentWidget = this.widget; // Définir le widget actuellement en cours de déplacement
+                this.initialX = e.clientX;
+                this.initialY = e.clientY;
+
+                document.addEventListener("mousemove", this.drag.bind(this));
+                document.addEventListener("mouseup", this.stopDragging.bind(this));
+            }
+
+            drag(e) {
+                if (this.currentWidget) {
+
+                const deltaX = e.clientX - this.initialX;
+                const deltaY = e.clientY - this.initialY;
+
+                if (e.clientX >= 90   && e.clientX <= 510) {
+                    this.widget.style.left = this.widget.offsetLeft + deltaX + "px";
+                    this.follower.style.left = this.widget.offsetLeft + deltaX + "px";
+                }
+
+                if (e.clientY >= 120   && e.clientY <= 780) {
+                    this.widget.style.top = this.widget.offsetTop + deltaY + "px";
+                    this.follower.style.top = this.widget.offsetTop + deltaY + 30 + "px";
+                }
+                this.initialX = e.clientX;
+                this.initialY = e.clientY;
+                }
+            }
+
+            stopDragging() {
+                this.currentWidget = null; // Réinitialiser le widget actuellement en cours de déplacement
+                document.removeEventListener("mousemove", this.drag);
+                document.removeEventListener("mouseup", this.stopDragging);
             }
         }
-    });
 
-    document.addEventListener("mouseup", () => {
-    isDragging = false;
-    });
-    */
+        const widgetPairs = [];
+        widgetPairs.push(new WidgetPair(document.getElementById("idBtn1"), document.getElementById("idName1")));
+        widgetPairs.push(new WidgetPair(document.getElementById("idBtn2"), document.getElementById("idName2")));
+        widgetPairs.push(new WidgetPair(document.getElementById("idBtn3"), document.getElementById("idName3")));
+   
     </script>
 
 </body>
